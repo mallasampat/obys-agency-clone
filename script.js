247ce20d6c30b78a784d44c6089ec5934ba9e39e
mallasampat <mallasampat@gmail.com>
2025-05-01 00:50:46 +0530

    Added Hero section, favicon

--- script.js
+++ script.js
@@ -1,41 +1,64 @@
-var tl = gsap.timeline();
+function loadingAnimation() {
+  var tl = gsap.timeline();
 
-tl.from(".line h1", {
-    y:150,
-    stagger:0.25,
-    duration:0.6,
-    delay:0.5
-})
-tl.from("#line1-part1", {
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+  });
+  tl.from("#line1-part1", {
     opacity: 0,
-    onStart: function() {
-        var h5Timer = document.querySelector("#line1-part1 h5");
-        var grow = 0;
-        setInterval(function() {
-            if(grow<100) {
-                h5Timer.innerHTML = grow++
-            } else {
-                h5Timer.innerHTML = grow
-            }
-        }, 33);
-    }
-});
-tl.to(".line h2", {
+    onStart: function () {
+      var h5Timer = document.querySelector("#line1-part1 h5");
+      var grow = 0;
+      setInterval(function () {
+        if (grow < 100) {
+          h5Timer.innerHTML = grow++;
+        } else {
+          h5Timer.innerHTML = grow;
+        }
+      }, 33);
+    },
+  });
+  tl.to(".line h2", {
     animationName: "anime",
-    opacity: 1
-})
-tl.to("#loader", {
+    opacity: 1,
+  });
+  tl.to("#loader", {
     opacity: 0,
-    duration:0.2,
-    delay:4
-})
-tl.from("#page1", {
+    duration: 0.2,
+    delay: 4,
+  });
+  tl.from("#page1", {
     delay: 0.2,
-    y : 1600,
-    opacity: 0,
+    y: 1600,
     ease: Power4,
-    duration:0.5
-})
-tl.to("#loader", {
-    display: "none"
-})
+    duration: 0.6,
+  });
+  tl.to("#loader", {
+    display: "none",
+  });
+  tl.from("#nav", {
+    opacity: 0
+  })
+  tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1 ", {
+    y: 120,
+    stagger: 0.2
+  })
+}
+
+loadingAnimation();
+
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#crsr", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+
+  Shery.makeMagnet("#nav-part2 h4");
+}
+
+cursorAnimation();
